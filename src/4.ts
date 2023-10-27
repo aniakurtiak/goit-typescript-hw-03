1bdc5e56798d2f03c9a235a3fad99bcf9defd2f2
Ania <124797307+aniakurtiak@users.noreply.github.com>
2023-10-28 01:42:37 +0300

    all done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,60 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+
+abstract class House {
+  door: boolean = false;
+  key: Key;
+  tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+        }
+    }
+}
+
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (this.key === key) {
+      this.door = true;
+      console.log("Wellcome, the door is open");
+    } else {
+      console.log("Sorry, your key doesn't match");
+    }
+  }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
